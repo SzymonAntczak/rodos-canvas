55220c7ca051b1a658193a7494aaf54d18b5fc58
Szymon Antczak <szymon0antczak@gmail.com>
2024-04-22 13:32:52 +0200

    Installing module federation

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "rodos-canvas",
+  exposes: {
+    "./Component": "./src/app/app.component.ts",
+  },
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
